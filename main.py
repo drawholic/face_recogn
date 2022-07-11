a22d3cce253fd00b4eaf4ac8d56dcfb4e4126ca6
drawholic <mamrokhavlad@gmail.com>
2022-07-11 13:40:53 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import sys
+
+cascPath = 'haarcascade_frontalface_default.xml'
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + cascPath)
+
+cap = cv2.VideoCapture(0)
+#
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+
+    cv2.imshow('preview', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
